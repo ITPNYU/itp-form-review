f7e275fdc44ac256fd7c88a168f8dd7b83c14ea2
Karl Ward <karl@ghostghost.net>
2014-03-12 09:18:56 -0400

    more angular testing --KW

--- itp-form-review.php
+++ itp-form-review.php
@@ -67,7 +67,7 @@ function ifr_page() {
   echo '<input type="search" ng-model="entryFilter" placeholder="search" />';
   echo '<ul>
   <li ng-repeat="e in entries | orderBy:date_created | filter:entryFilter">
-    {{e.id}}
+    {{e.id}} - {{e["1"]}}
   </li>
 </ul>';
 
