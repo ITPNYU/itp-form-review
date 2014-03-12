fa6ce7092f14ec2e9bbe162781e5c319b3a6881f
Karl Ward <karl@ghostghost.net>
2014-03-12 16:36:58 -0400

    WP JS --KW

--- itp-form-review.php
+++ itp-form-review.php
@@ -42,7 +42,7 @@ function ifr_gravity_public_key_callback() {
 function ifr_menu() {
   # $hookname is something like tools_page_itp-form-review
   $page_hook = add_management_page( 'Form Review', 'Form Review', 'manage_options', 'itp_form_review', 'ifr_page');
-  add_action('admin_print_scripts-' . $page_hook, 'ifr_script_load');
+  add_action('admin_enqueue_scripts-' . $page_hook, 'ifr_script_load');
 }
 
 function ifr_form_query($route) {
