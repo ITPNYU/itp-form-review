f6d6f5c4098889f9a55dbba2722167b14026de32
Karl Ward <karl@ghostghost.net>
2014-03-13 10:53:51 -0400

    WP JS --KW

--- itp-form-review.php
+++ itp-form-review.php
@@ -116,21 +116,18 @@ function ifr_page() {
 }
 
 function ifr_script_load($hook) {
-  echo ":::" . $hook . ":::";
-  if ('tools.php?page=itp_form_review' == $hook) {
-    wp_deregister_script('jquery-ui');
-    wp_register_script('jquery-ui','//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js',array('jquery'));
-    wp_enqueue_script('jquery-ui');
-    wp_enqueue_style('jquery-style', '//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css');
+  wp_deregister_script('jquery-ui');
+  wp_register_script('jquery-ui','//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js',array('jquery'));
+  wp_enqueue_script('jquery-ui');
+  wp_enqueue_style('jquery-style', '//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css');
 
-    wp_register_script('entriesAccordion', plugins_url('js/entriesAccordion.js', __FILE__), array('jquery-ui'));
-    wp_enqueue_script('entriesAccordion');
+  wp_register_script('entriesAccordion', plugins_url('js/entriesAccordion.js', __FILE__), array('jquery-ui'));
+  wp_enqueue_script('entriesAccordion');
 
-    wp_register_script('angular', '//ajax.googleapis.com/ajax/libs/angularjs/1.2.14/angular.min.js');
-    wp_register_script('angular-sanitize', '//ajax.googleapis.com/ajax/libs/angularjs/1.2.14/angular-sanitize.min.js', array('angular'));
-    wp_enqueue_script('angular');
-    wp_enqueue_script('angular-sanitize');
-  }
+  wp_register_script('angular', '//ajax.googleapis.com/ajax/libs/angularjs/1.2.14/angular.min.js');
+  wp_register_script('angular-sanitize', '//ajax.googleapis.com/ajax/libs/angularjs/1.2.14/angular-sanitize.min.js', array('angular'));
+  wp_enqueue_script('angular');
+  wp_enqueue_script('angular-sanitize');
 }
 
 function ifr_settings() {
