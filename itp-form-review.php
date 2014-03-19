e39f24b1c1d8767902c577894f023cad7c2d701d
Karl Ward <karl@ghostghost.net>
2014-03-19 15:19:20 -0400

    controller work --KW

--- itp-form-review.php
+++ itp-form-review.php
@@ -130,7 +130,7 @@ function ifr_page() {
     });
 
     ifrApp.controller("ReviewCtrl", function ($scope) {
-      $scope.reviews = [ {"id": "12", "reviews": { "reviewer": "abc123", "recommendation": "approve", "comment": "good", "date_created": "2014-03-19T19:17:32.353Z" } } ]; // FIXME: implement
+      $scope.reviews = [ {"id": "12", "reviews": [ { "reviewer": "abc123", "recommendation": "approve", "comment": "good", "date_created": "2014-03-19T19:17:32.353Z" } ] } ]; // FIXME: implement
 
       $scope.getReviews = function(id) {
         for (var rIndex in $scope.reviews) {
