1c963df4fffecc832a4373d4a7196ae7413155fb
Karl Ward <karl@ghostghost.net>
2014-03-20 13:47:03 -0400

    authn for api --KW

--- api/index.php
+++ api/index.php
@@ -2,29 +2,37 @@
 require '../lib/Slim/Slim/Slim.php';
 \Slim\Slim::registerAutoloader();
 
-/**
- * Step 2: Instantiate a Slim application
- *
- * This example instantiates a Slim application using
- * its default settings. However, you will usually configure
- * your Slim application now by passing an associative array
- * of setting names and values into the application constructor.
- */
-$app = new \Slim\Slim();
+$parse_uri = explode( 'wp-content', $_SERVER['SCRIPT_FILENAME'] );
+require_once( $parse_uri[0] . 'wp-load.php' );
 
-$app->get(
-    '/review',
-    function () {
-      echo "review GET";
-    }
-);
+if (current_user_can('activate_plugins')) { // indicates an administrator
+  /**
+   * Step 2: Instantiate a Slim application
+   *
+   * This example instantiates a Slim application using
+   * its default settings. However, you will usually configure
+   * your Slim application now by passing an associative array
+   * of setting names and values into the application constructor.
+   */
+  $app = new \Slim\Slim();
 
-$app->post(
-    '/review',
-    function () {
-        echo 'review POST';
-    }
-);
+  $app->get(
+      '/review',
+      function () {
+        echo "review GET";
+      }
+  );
 
-$app->run();
+  $app->post(
+      '/review',
+      function () {
+          echo 'review POST';
+      }
+  );
+
+  $app->run();
+}
+else {
+  http_response_code(403);
+}
 ?>
