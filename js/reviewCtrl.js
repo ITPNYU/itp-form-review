8a1aab1f34faa9dc5e8613e8084caadb5a1045d7
Karl Ward <karl@ghostghost.net>
2014-03-21 15:52:46 -0400

    JS bug fix --KW

--- js/reviewCtrl.js
+++ js/reviewCtrl.js
@@ -15,9 +15,9 @@ ifrApp.controller('ReviewCtrl', function ($scope, $http) {
   };
 
   $scope.submitReview = function(formId, entry, formData) {
-    //console.dir(formData);
     formData['form_id'] = formId;
     formData['entry'] = entry;
+    console.dir(formData);
     $http.post(ifr_api + 'review?blog=2', formData) // FIXME
       .success(function(data, status, headers, config) {
         console.log('success');
