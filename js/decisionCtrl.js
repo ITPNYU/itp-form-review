89d36022c478d5a4e4b19bbc64b3992e526c4249
Karl Ward <karl@ghostghost.net>
2014-03-24 12:02:02 -0400

    beginning of real decision controller --KW

--- js/decisionCtrl.js
+++ js/decisionCtrl.js
@@ -1,16 +1,38 @@
-ifrApp.controller("DecisionCtrl", function ($scope) {
-  $scope.decisions = {"12": "yes"}; // FIXME: implement
+ifrApp.controller("DecisionCtrl", function ($scope, $http) {
+  $http.get(ifr_api + 'decision?blog=2') // FIXME
+    .success(function(data) {
+      $scope.decisions = data.objects;
+    });
 
-  $scope.getDecision = function(id) {
-    return $scope.decisions[id];
+  $scope.getDecision = function(entry) {
+    for (var dIndex in $scope.decisions) {
+      if ($scope.decisions[dIndex]['entry'] === entry) {
+        return $scope.decisions[dIndex]['decision'];
+      }
+    }
   };
 
-  $scope.needsDecision = function(id) {
-    if ($scope.getDecision(id) != null) {
+  $scope.needsDecision = function(entry) {
+    if ($scope.getDecision(entry) != null) {
       return false;
     }
     else {
       return true;
     }
+  };
+
+  $scope.submitDecision = function(formId, entry, formData) {
+    //console.log('form ' + formId + ' entry ' + entry);
+    formData['form'] = formId;
+    formData['entry'] = entry;
+    console.dir(formData);
+    $http.post(ifr_api + 'decision?blog=2', formData) // FIXME
+      .success(function(data, status, headers, config) {
+        console.log('success ' + status + " " + data);
+        $scope.decisions.push(data); // FIXME: probably a better way
+      })
+      .error(function() {
+        console.log('error');
+      });
   };
 });
