64b516081386a0047f5da463281d36c7a193e817
Karl Ward <karl@ghostghost.net>
2014-03-31 11:59:29 -0400

    underscore testing --KW

--- js/decisionCtrl.js
+++ js/decisionCtrl.js
@@ -1,5 +1,4 @@
-ifrApp.controller("DecisionCtrl", ['$scope', '$http', '$window', 'underscore', 'ifr_decision_message',  
-  function ($scope, $http, $window, _, ifr_decision_message) {
+ifrApp.controller("DecisionCtrl", ['$scope', '$http', '$window', 'underscore', function ($scope, $http, $window, _) {
   $http.get(ifr_api + 'decision?blog=2') // FIXME
     .success(function(data) {
       $scope.decisions = data.objects;
