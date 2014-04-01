fd38cda6c9a61236cb7d054762117bd1976707f2
Karl Ward <karl@ghostghost.net>
2014-03-31 22:38:44 -0400

    testing register changes --KW

--- ifr_register_template.php
+++ ifr_register_template.php
@@ -24,6 +24,7 @@ elseif (isset($_REQUEST['email'])) {
     // lookup decision for that user
     $decision_result = $wpdb->get_row($wpdb->prepare("SELECT * FROM wp_2_ifr_decision WHERE user = %d AND (decision = 'approve' OR decision = 'comp') ", $user_result->id));
     $register_result = $wpdb->get_row($wpdb->prepare("SELECT * FROM wp_2_ifr_register WHERE user = %d", $user_result->id));
+    $payment_result = $wpdb->get_row($wpdb->prepare("SELECT * FROM FROM wp_2_ifr_payment WHERE form = %d AND entry = %s", array($register_result->form, $register_result->entry)));
     if (!$decision_result) {
 ?>
 <h2>Applicant not found, or applicant not yet accepted. If you wish to apply, please <a href="https://itp.nyu.edu/camp/2014/apply">apply here</a></h2>
@@ -34,14 +35,11 @@ elseif (isset($_REQUEST['email'])) {
   </form>
 <?php
     }
-    else if ($register_result) {
-      $payment_result = $wpdb->get_row($wpdb->prepare("SELECT * FROM FROM wp_2_ifr_payment WHERE form = %d AND entry = %s", array($register_result->form, $register_result->entry)));
-      if ($payment_result) {
+    else if ($register_result && $payment_result) {
 ?>
 <h2>Already Registered</h2>
 <p>Our records show that you have already registered and paid. Thanks! Stay tuned for more info about Camp soon!</p>
 <?php
-      }
     }
     else if (($decision_result->decision == 'comp') || 
       ($decision_result->decision == 'approve' && $decision_result->payment_due == 0)) {
