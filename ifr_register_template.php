061acf0f045198939320b6cdcb97bfba007f08cc
Karl Ward <karl@ghostghost.net>
2014-05-19 07:00:35 -0400

    HTML correction --KW

--- ifr_register_template.php
+++ ifr_register_template.php
@@ -1,8 +1,8 @@
 <?php 
 get_header();
 ?>
-<h2>System Maintenance<h2>
-<p>The NYU payment system is undergoing planned maintenance today, so we can't accept your payment today. Please try again this evening or tomorrow.  We apologize for the timing, it is not under ITP's control.</p>
+<h2>System Maintenance</h2>
+<p>The NYU payment system is undergoing planned maintenance right now, so we can't accept your payment today. Please try again this evening or tomorrow.  We apologize for the timing, it is not under ITP's control.</p>
 
 <h3>Questions? Problems?</h3>
 <p>If you need help, please contact us at (212) 998-1880 or email <a href="mailto:campinfo@itp.nyu.edu">campinfo@itp.nyu.edu</a>.</p>
