4a88f12ff09b15efd09eb5b11c859e49c12c6c60
Karl Ward <karl@ghostghost.net>
2014-06-05 13:26:38 -0400

    testing state handling for non-US --KW

--- ifr_register_template.php
+++ ifr_register_template.php
@@ -196,7 +196,7 @@ elseif (isset($_REQUEST['email'])) {
     <option value="YT">Canada - Yukon</option>
   </select>
   <input type="hidden" id="STATE" name="STATE" required />
-  <input type="text" id="STATEINPUT" value="NY" readonly />
+  <input type="text" id="STATEINPUT" value="NY" placeholder="enter non-US state" readonly />
   <label for="POSTAL_CODE">Postal Code</label>
   <input type="text" name="POSTAL_CODE" id="POSTAL_CODE" size="10" size="5" required="true" />
   <label for="COUNTRY">Country</label>
